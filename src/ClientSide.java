8315aca33f09e4836ef2b11cb81cca9f2f238b1c
Manish Sharma <sharmamanish16111999@gmail.com>
2022-07-18 09:05:43 +0530

    Made Some More changes

--- src/ClientSide.java
+++ src/ClientSide.java
@@ -21,7 +21,7 @@ public class ClientSide
 		if(choiceVar == 1)
 		{	
 			String pathOfFile = "C:\\Users\\Manish Sharma\\Documents\\abc.txt";
-			System.out.print("Enter Path of File : ");
+//			System.out.print("Enter Path of File : ");
 //			pathOfFile = sc.next();
 			try 
 			{
@@ -36,7 +36,7 @@ public class ClientSide
 		else if(choiceVar == 2)
 		{	
 			String fileName = "abc.txt";
-			System.out.print("Enter File Name With Extension : ");
+//			System.out.print("Enter File Name With Extension : ");
 //			fileName = sc.next();
 			try 
 			{
@@ -71,7 +71,7 @@ public class ClientSide
 		dataOutputStreamObj.write(fileNameInBytes); // sending fileName
 		
 		// Receiving file and creating a copy on client side
-		File fileObj = new File(fileName);
+		File fileObj = new File("C:\\Users\\Manish Sharma\\Downloads\\" + fileName + "_downloaded");
 		DataInputStream dataInputStreamObj = new DataInputStream(clientSocket.getInputStream());
 		
 		// Receiving file
